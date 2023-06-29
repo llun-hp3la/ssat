4adcaf72a95a483b795dd45e1fe55d3ea2e244c8
anh dang <dnhatanh@gmail.com>
2023-06-29 15:50:12 +0700

    basic main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 int
 main (int argc, char **argv)
-{        
-    struct ssat *volatile solver = solver_init();
-
-    return 0;
+{
+  struct ssat *volatile solver = solver_init();
+  // solver options are set in config file.
+  if (!solver)
+    error ("failed to initialize solver");
+  init_signal_handler ();
+  banner (); // print solver info
+  int res = sat_solve (solver);
+  reset_signal_handler ();
+  satch_release (solver);
+  return res;
 }

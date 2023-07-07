f0bbf57bfece764a0eded042efdfc8e5d1e3a141
anh dang <dnhatanh@gmail.com>
2023-07-07 11:12:09 +0700

    place holder for CDCL

---
+++ src/data_structures/solver_struct.h
@@ -0,0 +1,272 @@
+struct ssat {
+  
+};
+
+struct satch
+{
+  int status;			// UNKNOWN, SATISFIABLE, UNSATISFIABLE.
+  bool inconsistent;		// Empty clause found or derived.
+  bool iterate;			// Report learned unit clause.
+  bool stable;			// Stable mode (fewer restarts).
+  bool dense;			// Dense mode (connected - not watched).
+  unsigned level;		// Current decision level.
+  unsigned size;		// Number of variables.
+  size_t capacity;		// Allocated variables.
+  unsigned unassigned;		// Number of unassigned variables.
+  unsigned *levels;		// Decision levels of variables.
+  signed char *values;		// Current assignment of literals.
+  struct flags *flags;		// Variable flags.
+#ifndef NSAVE
+  signed char *saved;		// Saved assignments of variables.
+#endif
+#ifndef NTARGET
+  signed char *targets;		// Target phases.
+  unsigned target;		// Maximum trail size.
+#endif
+#ifndef NBEST
+  signed char *bests;		// Best phases.
+  unsigned best;		// Best trail size.
+#endif
+  signed char *marks;		// Mark flags of variables.
+#ifndef NLAZYACTIVATION
+  struct unsigned_stack put[2];	// To be put on decision queue / heap.
+#endif
+#ifndef NMINIMIZE
+  struct unsigned_stack poisoned;	// Variables marked as poisoned.
+  struct unsigned_stack removable;	// Variables marked as removable.
+#endif
+#ifndef NSHRINK
+  struct unsigned_stack shrunken;	// Shrunken variables.
+#endif
+#ifndef NCONTROL
+  unsigned *position;		// Variable position on trail.
+#endif
+  signed char *frames;		// Level pulled in learned clauses.
+#ifndef NQUEUE
+  struct queue queue[2];	// Variable decision queue (stable=1).
+#endif
+#ifndef NHEAP
+  struct heap scores[2];	// Variable decision heap (stable=1).
+#endif
+  struct clause **reasons;	// Reason clauses of a variable.
+#ifndef NBLOCK
+  struct clause binary[2];	// Temporary binary clauses.
+#endif
+#ifndef NBINARIES
+  struct unsigned_stack binaries;	// Saved redundant binary clauses.
+#endif
+#ifndef NELIMINATION
+  struct unsigned_stack extend;	// Solution reconstruction.
+  struct unsigned_stack resolvents;	// Temporary resolvents.
+#endif
+  struct watches *watches;	// Watches of a literal.
+  struct trail trail;		// Assigned literals.
+#ifndef NCONTROL
+  struct control control;	// control structure
+#endif
+  struct analyzed_stack analyzed;	// Analyzed literals.
+  struct unsigned_stack clause;	// Temporary clause.
+  struct unsigned_stack blocks;	// Analyzed decision levels.
+  struct clauses irredundant;	// Current irredundant clauses.
+  struct clauses redundant;	// Current redundant clauses.
+#ifndef NLIMITS
+  struct limits limits;		// Limits on restart, reduce, etc.
+#endif
+#ifndef NRELUCTANT
+  struct reluctant reluctant;	// Doubling for stable restart (Luby).
+#endif
+  struct options options;	// Few runtime options.
+  struct averages averages[2];	// Exponential moving averages (stable=1).
+  struct statistics statistics;	// Statistic counters.
+  struct profiles profiles;	// Built in run-time profiling.
+#ifndef NDEBUG
+  struct int_stack original;	// Copy of all original clauses.
+  struct checker *checker;	// Internal proof checker.
+#endif
+  struct int_stack added;	// Added external clause.
+  FILE *proof;			// Tracing to this file if non-zero.
+#ifdef LOGGING
+  char format[4][128];		// String buffer for logging.
+  unsigned next_format;		// Next buffer for logging.
+#endif
+#ifndef NVIVIFICATION
+  struct unsigned_stack sorted;
+  struct unsigned_stack sorter;
+  struct clauses vivification_schedule;
+  signed char *vivify_marks;	// TODO: make marks compatible with analyzed
+  // and mark_literal at the same time.
+  bool vivifying;
+#ifndef NDEBUG
+  bool watching;
+#endif
+#endif
+#ifdef DLIS
+  // When running DLIS we save how far the clauses are satisfied to avoid
+  // rechecking them. Our implementation is not optimal, since it does not
+  // handle chronological backtracking (so clauses will be rechecked several
+  // if there are satisfied by a literal set at a lower level.). However,
+  // this does not really matter, since you should not use DLIS anyway
+  // nowadays (or you need a less lazy SAT solver).
+  struct int_stack irred_sat_upto;
+  struct int_stack red_sat_upto;
+#endif
+};
+
+struct kissat
+{
+#if !defined(NDEBUG) || defined(METRICS)
+  bool backbone_computing;
+#endif
+#ifdef LOGGING
+  bool compacting;
+#endif
+  bool extended;
+  bool inconsistent;
+  bool iterating;
+  bool probing;
+#ifndef QUIET
+  bool sectioned;
+#endif
+  bool stable;
+#if !defined(NDEBUG) || defined(METRICS)
+  bool vivifying;
+#endif
+  bool watching;
+
+  bool large_clauses_watched_after_binary_clauses;
+
+  termination termination;
+
+  unsigned vars;
+  unsigned size;
+  unsigned active;
+
+  ints export;
+  ints units;
+  imports import;
+  extensions extend;
+  unsigneds witness;
+
+  assigned *assigned;
+  flags *flags;
+
+  mark *marks;
+
+  value *values;
+  phases phases;
+
+  eliminated eliminated;
+  unsigneds etrail;
+
+  links *links;
+  queue queue;
+
+  heap scores;
+  double scinc;
+
+  unsigned level;
+  frames frames;
+
+  unsigned_array trail;
+  unsigned *propagate;
+
+  unsigned best_assigned;
+  unsigned target_assigned;
+  unsigned unflushed;
+  unsigned unassigned;
+
+  unsigneds delayed;
+
+#if defined(LOGGING) || !defined(NDEBUG)
+  unsigneds resolvent;
+#endif
+  unsigned resolvent_size;
+  unsigned antecedent_size;
+
+  dataranks ranks;
+
+  unsigneds analyzed;
+  unsigneds levels;
+  unsigneds minimize;
+  unsigneds poisoned;
+  unsigneds promote;
+  unsigneds removable;
+  unsigneds shrinkable;
+
+  clause conflict;
+
+  bool clause_satisfied;
+  bool clause_shrink;
+  bool clause_trivial;
+
+  unsigneds clause;
+  unsigneds shadow;
+
+  arena arena;
+  vectors vectors;
+  reference first_reducible;
+  reference last_irredundant;
+  watches *watches;
+
+  sizes sorter;
+
+  generator random;
+  averages averages[2];
+  reluctant reluctant;
+
+  bounds bounds;
+  delays delays;
+  enabled enabled;
+  effort last;
+  limited limited;
+  limits limits;
+  waiting waiting;
+  unsigned walked;
+
+  statistics statistics;
+  mode mode;
+
+  uint64_t ticks;
+
+  format format;
+
+  statches antecedents[2];
+  statches gates[2];
+  patches xorted[2];
+  unsigneds resolvents;
+  bool resolve_gate;
+
+  struct kitten *kitten;
+#ifdef METRICS
+  uint64_t *gate_eliminated;
+#else
+  bool gate_eliminated;
+#endif
+  unsigneds sweep;
+
+#if !defined(NDEBUG) || !defined(NPROOFS)
+  unsigneds added;
+  unsigneds removed;
+#endif
+
+#if !defined(NDEBUG) || !defined(NPROOFS) || defined(LOGGING)
+  ints original;
+  size_t offset_of_last_original_clause;
+#endif
+
+#ifndef QUIET
+  profiles profiles;
+#endif
+
+#ifndef NOPTIONS
+  options options;
+#endif
+
+#ifndef NDEBUG
+  checker *checker;
+#endif
+
+#ifndef NPROOFS
+  proof *proof;
+#endif
+};
--- src/main.c
+++ src/main.c
@@ -1,14 +1,138 @@
+
+struct ssat *volatile solver;
+
+// The main CDCL loop in kissat, we wish to change the solver inner data structure so it can solve large problem, and communicate easily when runnning in parallel
+static int
+CDCL (kissat * solver)
+{
+  start_search (solver);
+  int res = solver->inconsistent ? 20 : 0;
+  while (!res)
+    {
+      clause *conflict = kissat_search_propagate (solver);
+      if (conflict)
+	res = kissat_analyze (solver, conflict);
+      else if (solver->iterating)
+	iterate (solver);
+      else if (!solver->unassigned)
+	res = 10;
+      else if (TERMINATED (search_terminated_1))
+	break;
+      else if (conflict_limit_hit (solver))
+	break;
+      else if (kissat_reducing (solver))
+	res = kissat_reduce (solver);
+      else if (kissat_switching_search_mode (solver))
+	kissat_switch_search_mode (solver);
+      else if (kissat_restarting (solver))
+	kissat_restart (solver);
+      else if (kissat_rephasing (solver))
+	kissat_rephase (solver);
+      else if (kissat_eliminating (solver))
+	res = kissat_eliminate (solver);
+      else if (kissat_probing (solver))
+	res = kissat_probe (solver);
+      else if (decision_limit_hit (solver))
+	break;
+      else
+	kissat_decide (solver);
+    }
+  stop_search (solver, res);
+  return res;
+}
+
+static int
+sat_solve (kissat * solver,
+		 int argc, char **argv, bool *cancel_alarm_ptr)
+{
+  *cancel_alarm_ptr = false;
+  if (argc == 2)
+    if (parsed_one_option_and_return_zero_exit_code (argv[1]))
+      return 0;
+  application application;
+  init_app (&application, solver);
+  bool ok = parse_options (&application, argc, argv);
+  if (application.time > 0)
+    *cancel_alarm_ptr = true;
+  if (!ok)
+    return 1;
+#ifndef QUIET
+  kissat_section (solver, "banner");
+  if (!GET_OPTION (quiet))
+    {
+      kissat_banner ("c ", SOLVER_NAME);
+      fflush (stdout);
+    }
+#endif
+#ifndef NPROOFS
+  if (!write_proof (&application))
+    return 1;
+#endif
+  if (!parse_input (&application))
+    {
+#ifndef NPROOFS
+      close_proof (&application);
+#endif
+      return 1;
+    }
+#ifndef QUIET
+#ifndef NOPTIONS
+  print_options (solver);
+#endif
+  print_limits (&application);
+  kissat_section (solver, "solving");
+#endif
+  int res = kissat_solve (solver);
+  if (res)
+    {
+      kissat_section (solver, "result");
+      if (res == 20)
+	{
+	  printf ("s UNSATISFIABLE\n");
+	  fflush (stdout);
+	}
+      else if (res == 10)
+	{
+#ifndef NDEBUG
+	  if (GET_OPTION (check))
+	    kissat_check_satisfying_assignment (solver);
+#endif
+	  printf ("s SATISFIABLE\n");
+	  fflush (stdout);
+	  if (application.witness)
+	    kissat_print_witness (solver,
+				  application.max_var, application.partial);
+	}
+    }
+#ifndef QUIET
+  kissat_print_statistics (solver);
+#endif
+#ifndef NPROOFS
+  close_proof (&application);
+#endif
+#ifndef QUIET
+  kissat_section (solver, "shutting down");
+  kissat_message (solver, "exit %d", res);
+#endif
+  return res;
+}
+
 int
 main (int argc, char **argv)
 {
-  struct ssat *volatile solver = solver_init();
+  solver = solver_init();
   // solver options are set in config file.
   if (!solver)
-    error ("failed to initialize solver");
-  init_signal_handler ();
+    error ("failed to initialize solver");    
+  kissat_init_alarm (kissat_alarm_handler);      
+  kissat_init_signal_handler (kissat_signal_handler);
   banner (); // print solver info
   int res = sat_solve (solver);
   reset_signal_handler ();
-  satch_release (solver);
+  sat_release (solver);
+#ifndef NDEBUG
+  if (!res)
+    return dump (0);
+#endif
   return res;
 }
